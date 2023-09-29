731d6829c4462278dc8ff8ec860effac911f746b
Anthony Bliss <anthony.bliss276@gmail.com>
2023-09-28 20:42:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ import pytz
 import requests
 import mysql.connector
 import boto3 
+import os
+import json
 from botocore.exceptions import ClientError
-from dotenv import load_dotenv
 
 
 def main(event, context):
@@ -24,23 +25,20 @@ def main(event, context):
     options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome('/opt/chromedriver',chrome_options=options)
-  
-    load_dotenv()
 
-    # Store values from .env file 
+    # Get environment variables
     LOGIN_URL = os.getenv('LOGIN_URL')
-    account = os.getenv('account')
-    username = os.getenv('username')
-    password = os.getenv('password')
+    account_info = os.getenv('account_info')
+    username_info = os.getenv('username_info')
+    password_info = os.getenv('password_info')
     reading_page = os.getenv('reading_page')
     zipcode = os.getenv('zipcode')
-    API_KEY = os.getenv('API Key')
-    T1_path = os.getenv("T1 xpath")
-    T2_path = os.getenv("T2 xpath")
-    T3_path = os.getenv("T3 xpath")
-    profile_icon_path = os.getenv("profile icon xpath")
-    menu_icon_path = os.getenv("menu icon xpath")
-    database = os.getenv("db")
+    API_KEY = os.getenv('API_Key')
+    T1_path = os.getenv('T1_xpath')
+    T2_path = os.getenv('T2_xpath')
+    T3_path = os.getenv('T3_xpath')
+    profile_icon_path = os.getenv('profile_icon_xpath')
+    menu_icon_path = os.getenv('menu_icon_xpath')
     
     # Go to login page
     driver.get(LOGIN_URL)
@@ -50,19 +48,16 @@ def main(event, context):
 
     # Wait for first field to be present then fill out all fields and login
     account_field = wait.until(EC.presence_of_element_located((By.NAME, "account")))
-    account_field.send_keys(account)
+    account_field.send_keys(account_info)
     username_field = driver.find_element(By.NAME, "username")
-    username_field.send_keys(username)
+    username_field.send_keys(username_info)
     password_field = driver.find_element(By.NAME, "password")
-    password_field.send_keys(password)
+    password_field.send_keys(password_info)
     password_field.send_keys(Keys.RETURN)
 
     # Navigate to screen where trailer readings are posted
-    next_screen_button = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "reading_page")))
+    next_screen_button = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, reading_page)))
     next_screen_button.click()
-
-    # Scrape content
-    page_content = driver.page_source
 
     # Get current date and time in US eastern time
     eastern = pytz.timezone('US/Eastern')
@@ -71,14 +66,14 @@ def main(event, context):
     current_time = current_datetime.strftime('%H:%M:%S')
 
     # OpenWeatherMap API to get the temperature
-    ZIP_CODE = 'zipcode'
+    ZIP_CODE = zipcode
     COUNTRY_CODE = 'us'
     URL = f"http://api.openweathermap.org/data/2.5/weather?zip={ZIP_CODE},{COUNTRY_CODE}&appid={API_KEY}&units=imperial"
     response = requests.get(URL)
     weather_data = response.json()
     temperature = weather_data['main']['temp']
 
-   # Initialize dictionary 
+   # Initialize data dictionary 
     data_dict = {
         "Date": current_date,
         "Time": current_time,
@@ -99,25 +94,20 @@ def main(event, context):
     # Loop through xpaths and extract the corresponding pressure readings, update dictionary
     for key, xpath in xpaths.items():
         try:
-            element = WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.XPATH, xpath)))
+            element = WebDriverWait(driver, 6).until(EC.presence_of_element_located((By.XPATH, xpath)))
             data_dict[key] = element.text
             data_dict['Offline'] = False
         except TimeoutException:
             print(f"{key} not found, setting to default value.")
 
-    # Depending on site size, profile icon may not be intially present
     def find_element_or_none(driver, by, value):
         try:
             return driver.find_element(by, value)
         except NoSuchElementException:
             return None
-        
-    # Initialize profile icon to check if present
-    profile_icon = find_element_or_none(driver, By.XPATH, profile_icon_path)
 
-    # If not found, check for menu icon
-    if not profile_icon:
-        menu_icon = find_element_or_none(driver, By.XPATH, menu_icon_path)
+    # Check for menu icon
+    menu_icon = find_element_or_none(driver, By.XPATH, menu_icon_path)
     
     # If menu icon is found, click it to reveal profile icon
     if menu_icon:
@@ -128,10 +118,8 @@ def main(event, context):
     if profile_icon:
         profile_icon.click()
     else:
-        print("Unable to logout!")
-
-    account_icon = wait.until(EC.element_to_be_clickable((By.ID, "top-profile-icon")))
-    account_icon.click()
+        account_icon = wait.until(EC.element_to_be_clickable((By.ID, "top-profile-icon")))
+        account_icon.click()
 
     # Logout of site
     logout_option = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Logout']")))
@@ -161,23 +149,17 @@ def main(event, context):
             raise e
         # Decrypts secret using the associated KMS key.
         secret = get_secret_value_response['SecretString']
+        return json.loads(secret)
 
-    secret_dict = get_secret()
-
-    if secret_dict:
-        username = secret_dict.get('username')
-        password = secret_dict.get('password')
-        host = secret_dict.get('host')
-        port = secret_dict.get('port')
-        
+    secret_dict = get_secret() 
 
     # Connect to database
     connection = mysql.connector.connect(
-        host,
-        port,
-        database,
-        username,
-        password,
+        host = secret_dict.get('host'),
+        port = secret_dict.get('port'),
+        database = secret_dict.get('db'),
+        username = secret_dict.get('username'),
+        password = secret_dict.get('password'),
         use_pure=True
     )
     cursor = connection.cursor()
